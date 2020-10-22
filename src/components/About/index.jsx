013ef26c8d458b7805f1f31a25ec5d07df551ca0
Norah Alharbi <nalharbi@waqood.sa>
2020-10-23 02:21:33 +0300

    init

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from '../shared/Header';
+import './styles.css';
 
 const Home = () => {
   return (
     <>
-      {/* You need to use your shared header component... */}
-      {/* You need a paragraph describing a fun fact about you */}
+      <Header title="About" />
+      <p>
+        I went to Japanese language school in Tokyo for two years and as well I lived in Japan
+         for five years. However, Now .. I can't even say two sentences easily!!!
+      </p>
     </>
   );
 }
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 
 // You need to import your shared header...
+import Header from "../shared/Header";
+import { Link } from 'react-router-dom';
+import './styles.css'
 
 const Home = () => {
   return (
     <>
-      {/* You need to use your shared header component... */}
-      {/* You need a link to your about page as a call to action */}
-    </>
+            <Header title="Welcome To Home!"/>
+            <p>Click on the under button to know a fun fact about me!</p>
+            <p>
+                <Link to="/about">
+                    <button type="button">
+                        About
+                    </button>
+                </Link>
+            </p>
+        </>
   );
 }
  
